2c43a8cb85f2663d9485ab9f0349dd620425011b
Insan2209 <michalwojtal5@wp.pl>
2025-02-04 15:21:06 +0100

    Logo change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         //returns div with img (logo) and site's name, there is also sign in button to the right
         <div className="flex flex-wrap w-full h-16 bg-palette1-d">
-            <img src="/img/Logo.png" alt="Pirate's Isle Logo" className="h-16 p-2"></img>
+            <img src="/img/Pirate's Isle Logo.png" alt="Pirate's Isle Logo" className="h-16 p-2"></img>
             <p className="font-semibold text-2xl md:text-3xl p-1 my-auto text-palette1-a font-lacquer">Pirate's Isle</p>
             <button className="ml-auto m-3 px-5 text-palette1-a bg-palette2-e hover:bg-palette2-b rounded-lg transition ease-in-out duration-500 font-lacquer">Sign In</button>
         </div>
