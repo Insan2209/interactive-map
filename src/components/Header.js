635fc11fc4344db27216c2f3a693260a927272a9
Insan2209 <michalwojtal5@wp.pl>
2025-02-05 12:11:50 +0100

    Changed sign in button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
         <div className="flex flex-wrap w-full h-16 bg-palette1-d">
             <img src="/img/Pirate's Isle Logo.png" alt="Pirate's Isle Logo" className="h-16 p-2"></img>
             <p className="font-semibold text-2xl md:text-3xl p-1 my-auto text-palette1-a font-lacquer">Pirate's Isle</p>
-            <button className="ml-auto m-3 px-5 text-palette1-a bg-palette2-e hover:bg-palette2-b rounded-lg transition ease-in-out duration-500 font-lacquer">Sign In</button>
+            <button id="signin" className="ml-auto m-3 px-5 text-palette1-a bg-palette2-e hover:bg-palette2-b rounded-lg transition ease-in-out duration-500 font-lacquer"><span>Sign In</span></button>
         </div>
     );  
 }
